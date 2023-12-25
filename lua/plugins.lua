8ca61d37d4a5428213970e68425c22694c1f88e4
Shin Imai <binaryninja0101@yahoo.com>
2023-12-25 18:46:26 +0000

    Cody plugin added

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -31,5 +31,17 @@ lvim.plugins = {
   build = 'make'
 },
 
+-- Sourcegraph
+{
+  "sourcegraph/sg.nvim",
+  dependencies = { "nvim-lua/plenary.nvim","nvim-telescope/telescope.nvim" },
+  config = function()
+    require("sg").setup({
+      enable_cody = true,
+      accept_tos = true,
+    })
+  end
+},
+
 }
 
